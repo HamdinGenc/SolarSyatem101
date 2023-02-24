b9bdad5b07f4544bd0d26c4f144b22ccba0866f6
hamdin <hamdingenc1989@gmail.com>
2023-02-23 20:50:56 -0800

    Solar System Basic Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        //write your code here
+      SolarSystem FactorX =new SolarSystem();
+      FactorX.Sun="1";
+      FactorX.moon="4";
+      FactorX.planet="2";
+      FactorX.Stars="10000";
+
+
+    }
+}
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SolarSystem {
+    String Sun;
+    String moon;
+    String planet;
+    String Stars;
+
+}
